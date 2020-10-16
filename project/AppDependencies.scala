38c1485e319180a6028ffef36aadbfbde5133bfe
Yang Wong <20066247+yangwong9664@users.noreply.github.com>
2020-10-16 10:54:44 +0100

    MTDSA-7338 update dependencies (#30)

--- project/AppDependencies.scala
+++ project/AppDependencies.scala
@@ -26,15 +26,15 @@ object AppDependencies {
     "uk.gov.hmrc"   %% "domain"            % "5.9.0-play-27",
     //"uk.gov.hmrc"   %% "play-hmrc-api"     % "4.1.0-play-26",
     "org.typelevel" %% "cats-core"         % "2.2.0",
-    compilerPlugin("com.github.ghik" % "silencer-plugin" % "1.7.0" cross CrossVersion.full),
-    "com.github.ghik" % "silencer-lib" % "1.7.0" % Provided cross CrossVersion.full,
+    compilerPlugin("com.github.ghik" % "silencer-plugin" % "1.7.1" cross CrossVersion.full),
+    "com.github.ghik" % "silencer-lib" % "1.7.1" % Provided cross CrossVersion.full,
     "com.chuusai"   %% "shapeless"         % "2.4.0-M1"
   )
 
   def test(scope: String = "test, it"): Seq[sbt.ModuleID] = Seq(
-    "org.scalatest"          %% "scalatest"          % "3.2.0"             % scope,
+    "org.scalatest"          %% "scalatest"          % "3.2.2"             % scope,
     "com.vladsch.flexmark"   % "flexmark-all"        % "0.36.8"           % scope,
-    "org.scalacheck"         %% "scalacheck"         % "1.14.2"            % scope,
+    "org.scalacheck"         %% "scalacheck"         % "1.14.3"            % scope,
     "org.scalamock"          %% "scalamock"          % "5.0.0"             % scope,
     "com.typesafe.play"      %% "play-test"          % PlayVersion.current % scope,
     "org.scalatestplus.play" %% "scalatestplus-play" % "4.0.3"             % scope,
