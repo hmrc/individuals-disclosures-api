3c90f650df3353422f56a3809695ace9562d49c6
Narendra Vijayarao <narendra.vijayarao@digital.hmrc.gov.uk>
2021-01-29 13:19:37 +0000

    Updated error handler

--- app/utils/ErrorHandler.scala
+++ app/utils/ErrorHandler.scala
@@ -88,8 +88,8 @@ class ErrorHandler @Inject()(
       case _: AuthorisationException => (UNAUTHORIZED, UnauthorisedError, "ClientError")
       case _: JsValidationException => (BAD_REQUEST, BadRequestError, "ServerValidationError")
       case e: HttpException => (e.responseCode, BadRequestError, "ServerValidationError")
-      case e: Upstream4xxResponse => (e.reportAs, BadRequestError, "ServerValidationError")
-      case e: Upstream5xxResponse => (e.reportAs, DownstreamError, "ServerInternalError")
+      case e: UpstreamErrorResponse if UpstreamErrorResponse.Upstream4xxResponse.unapply(e).isDefined => (e.reportAs, BadRequestError, "ServerValidationError")
+      case e: UpstreamErrorResponse if UpstreamErrorResponse.Upstream5xxResponse.unapply(e).isDefined => (e.reportAs, DownstreamError, "ServerInternalError")
       case _ => (INTERNAL_SERVER_ERROR, DownstreamError, "ServerInternalError")
     }
 
