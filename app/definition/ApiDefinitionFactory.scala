8982cc34f64bbb280fc26c1281b920727e4a4ea8
lukejones <47660116+BetaDraconis@users.noreply.github.com>
2021-02-10 15:18:16 +0000

    update-confidence-level

--- app/definition/ApiDefinitionFactory.scala
+++ app/definition/ApiDefinitionFactory.scala
@@ -34,12 +34,14 @@ class ApiDefinitionFactory @Inject()(appConfig: AppConfig) {
         Scope(
           key = readScope,
           name = "View your Self Assessment information",
-          description = "Allow read access to self assessment data"
+          description = "Allow read access to self assessment data",
+          confidenceLevel = 200
         ),
         Scope(
           key = writeScope,
           name = "Change your Self Assessment information",
-          description = "Allow write access to self assessment data"
+          description = "Allow write access to self assessment data",
+          confidenceLevel = 200
         )
       ),
       api = APIDefinition(
