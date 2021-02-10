fd8fb73e8067384bf68b50f8394db6a30a435c8b
lukejones <47660116+BetaDraconis@users.noreply.github.com>
2021-02-10 16:02:38 +0000

    Revert "update-confidence-level (#43)"
    This reverts commit 43847c692eb126ee5f35ffe9edb7e58d62cc1faa.

--- it/config/DocumentationControllerISpec.scala
+++ it/config/DocumentationControllerISpec.scala
@@ -30,14 +30,12 @@ class DocumentationControllerISpec extends IntegrationBaseSpec {
     |      {
     |         "key":"read:self-assessment",
     |         "name":"View your Self Assessment information",
-    |         "description":"Allow read access to self assessment data",
-    |         "confidenceLevel": 200
+    |         "description":"Allow read access to self assessment data"
     |      },
     |      {
     |         "key":"write:self-assessment",
     |         "name":"Change your Self Assessment information",
-    |         "description":"Allow write access to self assessment data",
-    |         "confidenceLevel": 200
+    |         "description":"Allow write access to self assessment data"
     |      }
     |   ],
     |   "api":{
