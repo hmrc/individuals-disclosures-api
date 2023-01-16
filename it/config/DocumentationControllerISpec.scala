76576a24ceb295d92a9ef7f3c7bf5fe7a7af7e30
Michael Olafisoye <molafisoye@gmail.com>
2023-01-16 10:54:33 +0000

    MTDSA-14004 refactor tests

--- it/config/DocumentationControllerISpec.scala
+++ it/config/DocumentationControllerISpec.scala
@@ -68,10 +68,16 @@ class DocumentationControllerISpec extends IntegrationBaseSpec {
   }
 
   "a documentation request" must {
-    "return the documentation" in {
+    "return the documentation when RAML" in {
       val response: WSResponse = await(buildRequest("/api/conf/1.0/application.raml").get())
       response.status shouldBe Status.OK
       response.body[String] should startWith("#%RAML 1.0")
+    }
+
+    "return the documentation when OAS" in {
+      val response: WSResponse = await(buildRequest("/api/conf/1.0/application.yaml").get())
+      response.status shouldBe Status.OK
+      response.body[String] should startWith("openapi: \"3.0.3\"")
     }
   }
 
